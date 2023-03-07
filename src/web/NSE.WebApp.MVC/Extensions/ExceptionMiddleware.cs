1f9d5e9476b1c91beb6ced0cf4658b18adb1634f
Felipe Morais <felipebazz@live.com>
2023-03-07 08:01:32 -0300

    Add project files.

---
+++ src/web/NSE.WebApp.MVC/Extensions/ExceptionMiddleware.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Refit;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace NSE.WebApp.MVC.Extensions
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (CustomHttpRequestException ex)
+            {
+                HandleRequestExceptionAsync(httpContext, ex.StatusCode);
+            }
+            catch (ValidationApiException ex)
+            {
+                HandleRequestExceptionAsync(httpContext, ex.StatusCode);
+            }
+            catch (ApiException ex)
+            {
+                HandleRequestExceptionAsync(httpContext, ex.StatusCode);
+            }
+        }
+
+        private static void HandleRequestExceptionAsync(HttpContext context, HttpStatusCode statusCode)
+        {
+            if (statusCode == HttpStatusCode.Unauthorized)
+            {
+                context.Response.Redirect($"/login?ReturnUrl={context.Request.Path}");
+                return;
+            }
+
+            context.Response.StatusCode = (int)statusCode;
+        }
+    }
+}
---
+++ src/web/NSE.WebApp.MVC/Services/CatalogoService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Options;
+using NSE.WebApp.MVC.Extensions;
+using NSE.WebApp.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Runtime;
+using System.Threading.Tasks;
+
+namespace NSE.WebApp.MVC.Services
+{
+    public class CatalogoService : Service, ICatalogoService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly AppSettings _settings;
+
+        public CatalogoService(HttpClient httpClient, IOptions<AppSettings> settings)
+        {
+            _httpClient = httpClient;
+            _settings = settings.Value;
+            _httpClient.BaseAddress = new Uri(_settings.CatalogoUrl);
+        }
+
+        public async Task<ProdutoViewModel> ObterPorId(Guid id)
+        {
+            var response = await _httpClient.GetAsync($"/catalogo/produtos/{id}");
+
+            TratarErrosResponse(response);
+
+            return await DeserializarObjetoResponse<ProdutoViewModel>(response);
+        }
+
+        public async Task<IEnumerable<ProdutoViewModel>> ObterTodos()
+        {
+            var response = await _httpClient.GetAsync("/catalogo/produtos");
+
+            TratarErrosResponse(response);
+
+            return await DeserializarObjetoResponse<IEnumerable<ProdutoViewModel>>(response);
+        }
+    }
+}
